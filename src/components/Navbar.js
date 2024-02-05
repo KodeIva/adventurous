07e2aaf5094c86c8f115fd34c7bdbeb820822f93
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-02-05 14:42:53 +0000

    Add container and home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Link }  from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="nav">
+     <div className="nav-container">
+      <Link to="/">ADVENTUROUS </Link>
+     </div>
+    </div>
   )
 }
 
