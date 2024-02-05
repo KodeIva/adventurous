97fa0824f9731d3d4b5a6fdd7458afadd02010bb
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-02-05 15:04:23 +0000

    Mob menu icons imported, onClick functionality given

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
+import {useState} from 'react'
 import { Link }  from 'react-router-dom'
 import {SiYourtraveldottv} from 'react-icons/si'
+import { LiaTimesSolid } from "react-icons/lia";
+import { AiOutlineBars } from "react-icons/ai";
 
 const Navbar = () => {
+ const [click,setClick] = useState(false)
+
   return (
     <div className="nav">
      <div className="nav-container">
       <Link to="/">ADVENTUROUS <SiYourtraveldottv/> </Link>
+      <div className="menu-icon">
+       <i onClick={() => setClick(!click)}>
+        {click ? <LiaTimesSolid /> : <AiOutlineBars/>}
+       </i>
+      </div>
      </div>
     </div>
   )
