4fd2f487ac774077f1d941714ab07f11b28b405b
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-02-06 21:23:20 +0000

    logo added as home link

--- src/components/Holidays.jsx
+++ src/components/Holidays.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {SiYourtraveldottv} from 'react-icons/si'
 
 const Holidays = () => {
   return (
-    <div>Holidays</div>
+    <div className='logo'>
+     <Link to="/">ADVENTUROUS <SiYourtraveldottv/> </Link>
+    </div>
   )
 }
 
