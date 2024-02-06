70dc028d0f1ab2db6123e1d79c5baa0ffdb8396a
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-02-06 21:09:53 +0000

    Components imported, created routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import CityBreaks from './components/CityBreaks';
+import Holidays from './components/Holidays';
+import Articles from './components/Articles';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navbar/>} />
-        
+        <Route path='/holidays' element={<Holidays/>} />
+        <Route path='/city-breaks' element={<CityBreaks/>} />
+        <Route path='/articles' element={<Articles/>} />
       </Routes>
     </BrowserRouter>
   );
