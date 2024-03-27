b70f186eb4925e455b18870fa5173dc1d10ef22e
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-03-27 16:25:20 +0000

    <Navbar/> taken out of routing, Home page created and added to routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 import CityBreaks from './components/CityBreaks';
 import Holidays from './components/Holidays';
@@ -8,8 +9,9 @@ import Articles from './components/Articles';
 function App() {
   return (
     <BrowserRouter>
+     <Navbar/>
       <Routes>
-        <Route path='/' element={<Navbar/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/holidays' element={<Holidays/>} />
         <Route path='/city-breaks' element={<CityBreaks/>} />
         <Route path='/articles' element={<Articles/>} />
