737334d8e09040656aae09d5c3d058f76487c9dc
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-03-28 14:54:07 +0000

    Text mobile sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className="nav flex flex-row p-3 w-[100%] h-[auto] justify-between " >
      <div className="nav-container">
        <Link to="/">
-        <span className='flex text-3xl p-2 text-blue-600'>
+        <span className='flex text-2xl xl:text-3xl p-2 text-blue-600'>
           <span className='text-blue-600'>ADVENTUR</span>  
           <span className='text-blue-800'><SiYourtraveldottv/></span> 
           <span className='text-blue-600'>US</span></span>
@@ -23,16 +23,16 @@ const Navbar = () => {
       </div>
       <div className='hidden md:flex w-[70%] justify-center '>
         <ul className='w-[80%] flex flex-row justify-center items-center text-blue-600'>
-          <li className='text-xl mx-4 '>
+          <li className=' text-base xl:text-xl mx-4'>
             <Link to="/">Home</Link>
           </li>
-           <li className='text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4'>
             <Link to="/">Holidays</Link>
           </li>
-           <li className='text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4'>
             <Link to="/">City Breaks</Link>
           </li>
-           <li className='text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4'>
             <Link to="/">Articles</Link>
           </li>
         </ul>
@@ -42,16 +42,17 @@ const Navbar = () => {
         </div>
       </div>  
    
-      <div className="menu-icon md:hidden w-[80%] h-[auto] bg-slate-100 ">
+      <div className="flex items-center menu-icon md:hidden w-[70%] h-[auto] bg-slate-100">
        <i className=' text-2xl w-10 h-10 absolute right-5' onClick={() => setClick(!click)}>
         {click 
            ? 
-           <LiaTimesSolid className='cursor-pointer z-10 text-5xl text-red-600 absolute right-5' /> 
+           <LiaTimesSolid className='cursor-pointer z-10 text-4xl text-red-600 absolute right-5' /> 
            : 
-           <AiOutlineBars className='cursor-pointer text-5xl text-blue-600 hover:rotate-180 absolute right-5 '/>}
+           <AiOutlineBars className='cursor-pointer text-4xl text-blue-600 hover:rotate-180 absolute right-5 '/>
+        }
        </i>
         {click && 
-         <ul className='mob-menu transition-all absolute right-5' style={{color: 'blue'}}>
+         <ul className='mob-menu transition-all absolute top-0 right-5' style={{color: 'blue'}}>
            <li className='mr-4 mt-40'>
             <Link to="/" onClick={() => setClick(!click) }>Home</Link>
            </li>
@@ -64,7 +65,8 @@ const Navbar = () => {
            <li className='mr-4 mt-14 mb-8'>
             <Link to="/articles" onClick={() => setClick(!click)}>Articles</Link>
            </li>
-         </ul>}
+         </ul>
+         }
       </div>
      </div>
   </>
