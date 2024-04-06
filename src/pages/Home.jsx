c0596de754fa7b36d5d33e42eec048c6070cf159
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-06 17:10:29 +0100

    Form styled, shadow moved to form div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
     <div className='home absolute justify-center text-center top-0 text-white'>
       <h1 className='text-2xl m-1 '>First Class Travel</h1>
       <h2 className='text-xl mb-2'>Top 1 Locations Worldwide</h2>
-      <form>
-        <div>
+      <form className='flex justify-center text-center'>
+        <div className='flex flex-row  bg-white shadow-md shadow-gray-600  rounded-2xl '>
           <input 
             type="text" 
             placeholder='Search Destinations...' 
-            className='w-[300px] md:w-[400px] p-2 shadow-md shadow-gray-600 rounded-xl '
+            className='w-[300px] md:w-[400px] p-2 '
              
           />
           <button className='flex justify-center text-center p-2 bg-white'><LuSearch className='text-2xl text-teal-600' /></button>
