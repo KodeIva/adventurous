154f1f2dd5f5e31331dd7914e04ec97322908bb5
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-06 16:59:14 +0100

    Button created , some styles added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 //import Hero from '../components/Hero'
 import '../styles/Home.css'
+import { LuSearch } from "react-icons/lu";
 
 const Home = () => {
   return (
     <div className='home absolute justify-center text-center top-0 text-white'>
       <h1 className='text-2xl m-1 '>First Class Travel</h1>
       <h2 className='text-xl mb-2'>Top 1 Locations Worldwide</h2>
-      <form action="">
+      <form>
         <div>
           <input 
             type="text" 
             placeholder='Search Destinations...' 
-            className='w-[300px] md:w-[400px] p-2 shadow-md shadow-gray-600 rounded-xl ' 
+            className='w-[300px] md:w-[400px] p-2 shadow-md shadow-gray-600 rounded-xl '
+             
           />
+          <button className='flex justify-center text-center p-2 bg-white'><LuSearch className='text-2xl text-teal-600' /></button>
         </div>
       </form>
       
