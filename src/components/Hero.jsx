b5dca69c31c5c00bef54094a7b002365f94993eb
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-06 12:42:17 +0100

    Added 'disablePictureInPicture' to the video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import beachLx from '../assets/beach-lx.mp4'
 const Hero = () => {
   return (
     <div className='w-full h-screen relative'>
-     <video autoPlay loop muted 
+     <video autoPlay loop muted  disablePictureInPicture
        className='w-full h-full object-cover' 
        src={beachLx}
      />
