1bf7553f3d964ad32b84fdcc3eba1281f33b6d79
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-06 16:52:45 +0100

    Added form with input, styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,15 @@ import '../styles/Home.css'
 const Home = () => {
   return (
     <div className='home absolute justify-center text-center top-0 text-white'>
-      <h1 className='text-2xl'>First Class Travel</h1>
-      <h2 className='text-xl'>Top 1 Locations Worldwide</h2>
+      <h1 className='text-2xl m-1 '>First Class Travel</h1>
+      <h2 className='text-xl mb-2'>Top 1 Locations Worldwide</h2>
       <form action="">
         <div>
-          <input type="text" placeholder='Search Destinations...' className='p-3' />
+          <input 
+            type="text" 
+            placeholder='Search Destinations...' 
+            className='w-[300px] md:w-[400px] p-2 shadow-md shadow-gray-600 rounded-xl ' 
+          />
         </div>
       </form>
       
