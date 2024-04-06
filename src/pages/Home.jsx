10ce2b984fa9e984287e7cf47bd77a975f893a71
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-06 16:22:37 +0100

    Tailwind configuration changed, styles are properly working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,14 @@ import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div className='home absolute top-0'>
+    <div className='home absolute justify-center text-center top-0 text-white'>
+      <h1 className='text-2xl'>First Class Travel</h1>
+      <h2 className='text-xl'>Top 1 Locations Worldwide</h2>
+      <form action="">
+        <div>
+          <input type="text" placeholder='Search Destinations...' className='p-3' />
+        </div>
+      </form>
       
     </div>
   )
