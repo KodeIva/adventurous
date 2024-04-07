a3d8b2a8919d83d9cfe78864beda82aed8ef6889
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-07 15:10:31 +0100

    Input and button borders round changed,form a,d div given styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,15 @@ const Home = () => {
   return (
     <div className='home absolute justify-center text-center top-0 text-white'>
       <h1 className='text-2xl m-1 '>First Class Travel</h1>
-      <h2 className='text-xl mb-2'>Top 1 Locations Worldwide</h2>
+      <h2 className='text-xl mb-2'>Top Locations Worldwide</h2>
       <form className='flex justify-center text-center'>
-        <div className='flex flex-row  bg-white shadow-md shadow-gray-600  rounded-2xl '>
+        <div className='flex flex-row shadow-md shadow-gray-600  rounded-2xl '>
           <input 
             type="text" 
             placeholder='Search Destinations...' 
-            className='w-[300px] md:w-[400px] p-2 '
-             
+            className='w-[300px] md:w-[400px] p-2 outline-none rounded-tl-md rounded-bl-md bg-gray-50/90 text-slate-700'  
           />
-          <button className='flex justify-center text-center p-2 bg-white'><LuSearch className='text-2xl text-teal-600' /></button>
+          <button className='flex justify-center text-center text-slate-800 p-2 px-3 bg-white rounded-br-md rounded-tr-md'><LuSearch className='text-2xl text-teal-600' /></button>
         </div>
       </form>
       
