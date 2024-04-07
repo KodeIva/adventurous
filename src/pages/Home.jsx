d7e886122c9f4f8c2bf1adcf5daa2518ef798643
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-07 16:25:15 +0100

    imported useState, created const 'inputValue', added value and onChange to input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 //import Hero from '../components/Hero'
 import '../styles/Home.css'
 import { LuSearch } from "react-icons/lu";
 
 const Home = () => {
+  const [inputValue,setInputValue] = useState('')
+
   return (
     <div className='home absolute justify-center text-center top-0 text-white'>
       <h1 className='text-2xl m-1 '>First Class Travel</h1>
@@ -14,8 +17,15 @@ const Home = () => {
             type="text" 
             placeholder='Search Destinations...' 
             className='w-[300px] md:w-[400px] p-2 outline-none rounded-tl-md rounded-bl-md bg-gray-50/90 text-slate-700'  
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-          <button className='flex justify-center text-center text-slate-800 p-2 px-3 bg-white rounded-br-md rounded-tr-md'><LuSearch className='text-2xl text-teal-600 hover:scale-125 transition-all duration-500 ' /></button>
+          <button 
+            type='submit'
+            className='flex justify-center text-center text-slate-800 p-2 px-3 bg-white rounded-br-md rounded-tr-md'
+            >
+            <LuSearch className='text-2xl text-teal-600 hover:scale-125 transition-all duration-500  ' />
+          </button>
         </div>
       </form>
       
