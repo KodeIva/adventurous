081de96d21a07cbbfe4f277763e8e0d5e5985961
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-10 14:25:44 +0100

    Main div and title added,styled

--- src/pages/Holidays.jsx
+++ src/pages/Holidays.jsx
@@ -4,8 +4,8 @@ import {SiYourtraveldottv} from 'react-icons/si'
 
 const Holidays = () => {
   return (
-    <div className='logo'>
-     <Link to="/">ADVENTUROUS <SiYourtraveldottv/> </Link>
+    <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
+     <h1>All Inclusive Resorts</h1>
     </div>
   )
 }
