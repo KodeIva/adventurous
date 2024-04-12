ce955abdfd765c2a69ce1a5f349f16541a2b012d
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-12 16:36:34 +0100

    Added images hover effect, h1 styled

--- src/pages/Holidays.jsx
+++ src/pages/Holidays.jsx
@@ -11,13 +11,13 @@ import Cyprus from '../assets/cyprus.jpg'
 const Holidays = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
-     <h1>All Inclusive Resorts</h1>
+     <h1 className='text-3xl my-2  text-teal-600 tracking-wide  '>All Inclusive Resorts</h1>
      <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
-        <img className='col-span-2 md:col-span-3 row-span-2 w-full' src={Carribean} alt={Carribean} />
-        <img className='h-[100%]' src={Tahiti} alt={Tahiti} />
-        <img className='h-[100%]' src={Maldives} alt={Maldives} />
-        <img className='h-[100%]' src={Thailand} alt={Thailand} />
-        <img className='h-[100%]' src={Cyprus} alt={Cyprus} />
+        <img className='col-span-2 md:col-span-3 row-span-2 w-full hover:contrast-150 transition-all duration-500' src={Carribean} alt={Carribean} />
+        <img className='h-[100%] hover:contrast-150 transition-all duration-500' src={Tahiti} alt={Tahiti} />
+        <img className='h-[100%] hover:contrast-150 transition-all duration-500' src={Maldives} alt={Maldives} />
+        <img className='h-[100%] hover:contrast-150 transition-all duration-500' src={Thailand} alt={Thailand} />
+        <img className='h-[100%] hover:contrast-150 transition-all duration-500' src={Cyprus} alt={Cyprus} />
      </div>
     </div>
   )
