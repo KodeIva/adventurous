3e710822356ada67dbcb315cb04daece1f7eb0c5
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-20 17:34:04 +0100

    Links styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className="flex w-full h-auto p-3 bg-teal-700" >
+    <div className="flex w-full h-auto p-3 bg-teal-200" >
      <ul className="flex flex-row w-full h-auto p-9" >
-          <li className='gb-rink-700 text-base xl:text-xl mx-4'>
+          <li className='gb-rink-700 text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide'>
             <Link to="/">Home</Link>
           </li>
-           <li className='text-base xl:text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide'>
             <Link to="/holidays">Holidays</Link>
           </li>
-           <li className='text-base xl:text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide'>
             <Link to="/city-breaks">City Breaks</Link>
           </li>
-           <li className='text-base xl:text-xl mx-4'>
+           <li className='text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide'>
             <Link to="/articles">Articles</Link>
           </li>
         </ul>
