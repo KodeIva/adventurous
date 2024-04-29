434a2f2d23825458a7f8eba4ae944298d61cfef9
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-29 10:45:55 +0100

    Added link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className="flex w-full h-auto p-3 bg-teal-200" >
+    <div className="flex flex-col w-full h-auto p-3 bg-teal-200" >
      <ul className="flex flex-row w-full h-auto justify-center text-center p-9 " >
           <li className='gb-rink-700 text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide hover:text-blue-800 hover:font-semibold hover:underline hover:leading-4 hover:decoration-2 transition duration-700 ease-in-out'>
             <Link to="/">Home</Link>
@@ -18,7 +18,12 @@ const Footer = () => {
             <Link to="/articles">Articles</Link>
           </li>
         </ul>
-        <div><span>{new Date().getFullYear()} @KodeIva</span></div>
+        <div>
+          <span>
+            {new Date().getFullYear()} 
+            <a href="https://kodeiva.netlify.app" target='blank'>@KodeIva</a> 
+          </span>
+          </div>
     </div>
   )
 }
