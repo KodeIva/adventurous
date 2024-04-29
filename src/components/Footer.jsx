259438f26af80e0bd41075000900816a660cca7c
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2024-04-29 10:57:12 +0100

    styles to div

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className="flex flex-col w-full h-auto p-3 bg-teal-200" >
+    <div className="flex flex-col justify-center text-center w-full h-auto p-3 bg-teal-200" >
      <ul className="flex flex-row w-full h-auto justify-center text-center p-9 " >
           <li className='gb-rink-700 text-base xl:text-xl mx-4 text-blue-500 bold tracking-wide hover:text-blue-800 hover:font-semibold hover:underline hover:leading-4 hover:decoration-2 transition duration-700 ease-in-out'>
             <Link to="/">Home</Link>
@@ -18,10 +18,16 @@ const Footer = () => {
             <Link to="/articles">Articles</Link>
           </li>
         </ul>
-        <div>
-          <span>
-            {new Date().getFullYear()} 
-            <a href="https://kodeiva.netlify.app" target='blank'>@KodeIva</a> 
+        <div className='text-blue-600'>
+          <span> @ Copyright 
+            <span> {new Date().getFullYear()} - </span> 
+            <a 
+              href="https://kodeiva.netlify.app" 
+              target='blank'
+              className='font-serif text-lg font-medium tracking-wider'
+            >
+             KodeIva
+            </a> 
           </span>
           </div>
     </div>
