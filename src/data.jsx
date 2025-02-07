18ac5ee2d1c63789c23b985dfc735f42fcd28729
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-02-07 17:09:57 +0000

    new file 'data.jsx' created, destinations data added

---
+++ src/data.jsx
@@ -0,0 +1,34 @@
+import Carribean from '../assets/caribbean.jpg'
+import Tahiti from '../assets/tahiti.jpg'
+import Maldives from '../assets/maldives.jpg'
+import Thailand from '../assets/thailand.jpg'
+import Cyprus from '../assets/cyprus.jpg'
+
+export const destinations = [
+    {
+        id: 1,
+        image: Carribean,
+        info: 'Situated largely on the Caribbean Plate, the region has more than 700 islands, islets, reefs, and cays. Island arcs delineate the northern and eastern edges of the Caribbean Sea:[7] the Greater Antilles in the north and the Lesser Antilles, which includes the Leeward Antilles, in the east and south. The nearby Lucayan Archipelago, comprising The Bahamas and the Turks and Caicos Islands, is considered to be a part of the Caribbean despite not bordering the Caribbean Sea. All the islands in the Antilles plus the Lucayan Archipelago form the West Indies, which is often interchangeable with the term Caribbean.'
+    },
+    {
+        id: 2,
+        image: Tahiti,
+        info: 'Tahiti is the largest island in French Polynesia, the South Pacific archipelago. Shaped like a figure-8, it is divided into Tahiti Nui (the larger, western section) and Tahiti Iti (the eastern peninsula). With black-sand beaches, lagoons, waterfalls and 2 extinct volcanoes, it is a popular vacation destination. Explored by Captain James Cook in the 18th century, it was also often painted by French artist Paul Gauguin.'
+    },
+    {
+        id: 3,
+        image: Maldives,
+        info: 'The Maldives is a republic lies south-west of the Indian sub-continent. It is made up of a chain of nearly 1,200 islands, most of them uninhabited.None of the coral islands stand more than 1.8 metres (six feet) above sea level, making the country vulnerable to any rise in sea levels associated with global warming.'
+    },
+    {
+        id: 4,
+        image: Thailand, 
+        info: 'Thailand, country located in the centre of mainland Southeast Asia. Located wholly within the tropics, Thailand encompasses diverse ecosystems, including the hilly forested areas of the northern frontier, the fertile rice fields of the central plains, the broad plateau of the northeast, and the rugged coasts along the narrow southern peninsula.'
+    },
+    {
+        id: 5,
+        image: Cyprus,
+        info: 'Cyprus, island in the eastern Mediterranean Sea renowned since ancient times for its mineral wealth, superb wines and produce, and natural beauty.'
+    }
+   
+]
