9573b64ce6440f08eb1c225889e577c32d032d88
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-01 18:00:58 +0000

    <label>, <input> styled

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,12 +5,12 @@ const SignIn = () => {
     <form action="" className=' bg-red-300 w-full h-[100vh] sm:bg-yellow-200 lg:bg-orange-500 flex justify-center items-center'>
         <div className='bg-slate-500 w-[80%] sm:w-[60%] lg:w-[40%] xl:w-[30%] h-auto py-9 px-7 rounded-xl'>
           <div className="row flex flex-col w-[100%] mb-6 ">
-            <label htmlFor="">Email</label>
-            <input type="text" />
+            <label className="text-white tracking-wider mb-1 pl-2" htmlFor="">Email</label>
+            <input className='h-10 rounded-xl outline-none' type="text" />
           </div>
           <div className="row flex flex-col w-[100%] mb-4">
-            <label htmlFor="">Password</label>
-            <input type="text" />
+            <label className='text-white tracking-wider mb-1 pl-2' htmlFor="">Password</label>
+            <input className='h-10 rounded-xl outline-none' type="text" />
           </div>
         </div>
      
