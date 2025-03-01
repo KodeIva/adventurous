9bbcac209f8541e94d049eabf047e9ee42a856b2
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-01 11:43:04 +0000

    Sign in page created,form with divs,labels and inputs added

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <form action="">
+        <div className="row">
+            <label htmlFor="">Email</label>
+            <input type="text" />
+        </div>
+        <div className="row">
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+    </form>
+  )
+}
+
+export default SignIn
