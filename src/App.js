07eef76c1d6ae06c28ce6450386ec8cce08b8ff2
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-01 14:12:04 +0000

    SignIn Route path created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Holidays from './pages/Holidays';
 import Articles from './pages/Articles';
 import Footer from './components/Footer';
 import ScrollToTop from 'react-scroll-to-top';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/holidays' element={<Holidays/>} />
         <Route path='/city-breaks' element={<CityBreaks/>} />
         <Route path='/articles' element={<Articles/>} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
       <ScrollToTop smooth color='white' style={{display: 'flex', color: 'lightgreen', backgroundColor: 'lightgreen', justifyContent: 'center',borderRadius: '15%', alignItems: 'center',fontSize: '900', width: '50px'}}/>
       <Footer />
