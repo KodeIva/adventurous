9f33c37a218047524d7aa7133b747dd52be404a3
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-01 20:34:11 +0000

    Hover added to <button> 'Sign in' and  <button> 'Sign up now!'

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -14,11 +14,11 @@ const SignIn = () => {
           </div>
           <button className='h-10 rounded-xl outline-none  text-slate-500 bg-white w-[100%]  border-2 my-8 font-semibold tracking-wider hover:bg-slate-500 hover:text-white hover:border-white duration-500'>Sign in</button>
           <div>
-            <button className='pr-[20px] text-sm text-blue-400'>Forgot Password?</button>
+            <button className='pr-[20px] text-sm text-blue-300 hover:text-white duration-500'>Forgot Password?</button>
           </div>
           <div className='mt-9'>
             <span className='text-sm text-white'>Need an account? </span>
-            <button className='text-sm text-blue-400'> Sign up now!</button>
+            <button className='text-sm text-blue-300 hover:text-white duration-500'> Sign up now!</button>
           </div>
         </div>
     </form>
