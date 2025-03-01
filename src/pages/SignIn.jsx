a7bbcf04aaa53ca41f20905d14a09c1528cdefbf
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-01 16:34:31 +0000

    <form>, <div> styled

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <form action="">
-        <div className="row">
+    <form action="" className='w-full h-[70vh] flex justify-center items-center'>
+        <div className='bg-slate-500 w-[80%] h-auto py-9 px-7 rounded-xl'>
+          <div className="row flex flex-col mb-6 ">
             <label htmlFor="">Email</label>
             <input type="text" />
-        </div>
-        <div className="row">
-            <label htmlFor=""></label>
+          </div>
+          <div className="row flex flex-col mb-4">
+            <label htmlFor="">Password</label>
             <input type="text" />
+          </div>
         </div>
+     
     </form>
   )
 }
