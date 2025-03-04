962fca80b2a5ae2c522ba79f0e35ca180d689c67
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-04 11:40:12 +0000

    'handleSubmit' function created and added to onSubmit event <form>, input onChange function added, console returns email and password

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,8 +4,13 @@ const SignIn = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email,password);
+  }
+
   return (
-    <form action="" className='w-full h-[100vh] flex justify-center items-center'>
+    <form onSubmit={handleSubmit} className='w-full h-[100vh] flex justify-center items-center'>
         <div className='bg-slate-500 w-[80%] sm:w-[60%] lg:w-[40%] xl:w-[30%] h-auto py-9 px-7 mt-[-150px] rounded-xl'>
           <div className="row flex flex-col w-[100%] mb-6 ">
             <label className="label" htmlFor="email">Email</label>
@@ -13,7 +18,9 @@ const SignIn = () => {
                className='h-10 rounded-xl outline-none'
                type="text" 
                id='email'
+               name='email'
                value={email}   
+               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="row flex flex-col w-[100%] mb-4">
@@ -22,7 +29,9 @@ const SignIn = () => {
               className='h-10 rounded-xl outline-none' 
               type="text" 
               id='password'
+              name='password'
               value={password}
+              onChange={(e) => setPassword(e.target.value)}
               />
           </div>
           <button className='h-10 rounded-xl outline-none  text-slate-500 bg-white w-[100%]  border-2 my-8 font-semibold tracking-wider hover:bg-slate-500 hover:text-white hover:border-white duration-500'>Sign in</button>
