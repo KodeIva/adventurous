9391a8b86622f4e9ae5d7e2abe35219d443d7e2f
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-04 11:24:53 +0000

    <label> tailwind custom style class created in index.css

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <form action="" className='w-full h-[100vh] flex justify-center items-center'>
         <div className='bg-slate-500 w-[80%] sm:w-[60%] lg:w-[40%] xl:w-[30%] h-auto py-9 px-7 mt-[-150px] rounded-xl'>
           <div className="row flex flex-col w-[100%] mb-6 ">
-            <label className="label" htmlFor="">Email</label>
-            <input className='h-10 rounded-xl outline-none' type="text" />
+            <label className="label" htmlFor="email">Email</label>
+            <input 
+               className='h-10 rounded-xl outline-none'
+               type="text" 
+               id='email'
+               value={email}   
+            />
           </div>
           <div className="row flex flex-col w-[100%] mb-4">
-            <label className='label' htmlFor="">Password</label>
-            <input className='h-10 rounded-xl outline-none' type="text" />
+            <label className='label' htmlFor="password">Password</label>
+            <input 
+              className='h-10 rounded-xl outline-none' 
+              type="text" 
+              id='password'
+              value={password}
+              
+              />
           </div>
           <button className='h-10 rounded-xl outline-none  text-slate-500 bg-white w-[100%]  border-2 my-8 font-semibold tracking-wider hover:bg-slate-500 hover:text-white hover:border-white duration-500'>Sign in</button>
           <div>
