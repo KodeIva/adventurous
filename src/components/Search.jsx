661e984ef54772477f38887c03a5f1d98d4453a0
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-03-30 19:00:20 +0100

    <form> with <div>-destinations, <div> for check in/out added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,8 +7,31 @@ const Search = () => {
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta rerum corporis repellendus esse ratione iste velit in aperiam dolores facere, ad autem quas, asperiores officiis architecto cumque? Exercitationem a quasi ab reiciendis asperiores saepe debitis laudantium? Reiciendis fuga accusamus voluptas voluptates repellendus recusandae, accusantium placeat a suscipit, dicta minus iure!
     </div>
     <div>
-        <h2>Firts time booking - get 15% off</h2>
-        <button>Book noe and save</button>
+        <div>
+             <h2>Firts time booking - get 15% off</h2>
+             <button>Book now and save</button>
+        </div>
+        <form>
+            <div>
+                <label htmlFor="">Destinations</label>
+                <select>
+                    <option>Carribean</option>
+                    <option>Tahiti</option>
+                    <option>Maldives</option>
+                    <option>Thailand</option>
+                    <option>Cypres</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="">Check-in</label>
+                <input type="date" />
+            </div>
+            <div>
+                <label htmlFor="">Check-out</label>
+                <input type="date" />
+            </div>
+        </form>
+       
     </div>
    </section>
   )
