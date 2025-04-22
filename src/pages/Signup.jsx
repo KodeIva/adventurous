c3ecf9f162c75c9047f21ae8612ac7c8c565ddea
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-04-22 12:32:25 +0100

    <h4> - title moved to section, <h4> and <form> styles added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 
 const Signup = () => {
   return (
-   <section className="flex justify-center bg-blue-50 w-full h-auto py-5"> 
-    <form method="POST" className="bg-red-400 w-[80%] md:w-[50%] lg:w-[30%]">
+   <section className="flex flex-col items-center justify-center bg-blue-50 w-full h-auto py-5 mb-20"> 
+    <h4 className="text-3xl text-slate-600 mb-3">Register</h4>
+    <form method="POST" className="bg-slate-500 w-[80%] sm:w-[60%] lg:w-[40%] xl:w-[30%] h-auto py-9 px-7 rounded-xl">
       <div>
-        <h4>Register</h4>
+        
         <div className="flex flex-col">
           <label className="mt-5 mb-2" htmlFor="">Username</label>
           <input className="border-2 border-md border-slate-400 rounded-md h-12 pl-5 " type="text" required/>
