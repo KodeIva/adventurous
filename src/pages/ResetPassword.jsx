db48ac55d003ae3bfa4a2714a686ee35c67c4a73
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-04-25 19:56:34 +0100

    text added, styling

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -11,8 +11,9 @@ const ResetPassword = () => {
 
   return (
         <section className='flex flex-col items-center justify-center bg-blue-50 w-full h-auto py-5'> 
-         <h4 className="text-3xl text-slate-600 mt-5 mb-[-26px]">Reset Password</h4>
-         <form onSubmit={handleSubmit} className='w-full h-[100vh] flex justify-center items-center'>
+         <h4 className="text-3xl text-slate-600 mt-7">Reset Password</h4>
+         <p className='pt-5'>Reset your password by providing your registered email below. </p>
+         <form onSubmit={handleSubmit} className='w-full h-[100vh] flex justify-center items-center mt-[-20px]'>
            
              <div className='bg-slate-500 w-[80%] sm:w-[60%] lg:w-[40%] xl:w-[30%] h-auto py-9 px-7 mt-[-150px] rounded-xl'>
                <div className="row flex flex-col w-[100%] mb-6 ">
@@ -29,7 +30,7 @@ const ResetPassword = () => {
                
                  <button 
                    type='submit' 
-                    className='h-10 rounded-xl outline-none  text-slate-500 bg-white w-[100%]  border-2 my-8 font-semibold tracking-wider hover:bg-slate-500 hover:text-white hover:border-white duration-500'
+                    className='h-10 rounded-xl outline-none  text-slate-500 bg-white w-[100%]  border-2 my-6 font-semibold tracking-wider hover:bg-slate-500 hover:text-white hover:border-white duration-500'
                  >
                    Next
                 </button>
