e7e417b8dc68e7b47701cdb5453f5645e8355b97
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2025-05-09 16:48:27 +0100

    more styling added

--- src/pages/CityBreaks.jsx
+++ src/pages/CityBreaks.jsx
@@ -4,28 +4,28 @@ import { BsCalendar4Range } from "react-icons/bs";
 
 const CityBreaks = () => {
   return (
-    <section className='w-full h-auto lg:bg-yellow-300 xl:bg-orange-500'>
+    <section className='w-full h-auto'>
       <div className='flex flex-col justify-center items-center'>
-        <h2 className='text-2xl lg:text-3xl text-slate-700 font-medium'>Search City Bbreaks Packages</h2>
+        <h2 className='text-2xl my-5 lg:text-3xl text-slate-700 font-semibold'>Search City Bbreaks Packages</h2>
         <form action="" className='flex flex-col lg:flex-row'>
-          <label htmlFor="" className='flex justify-center items-center w-80 lg:w-56 xl:w-64  h-12 m-1 border-2 rounded-md border-slate-600'>
+          <label htmlFor="" className='flex justify-center items-center mt-3 w-80 lg:w-56 xl:w-64  h-12 m-1 border-2 rounded-md border-slate-600'>
             <IoLocationOutline className='text-3xl' /> 
-            <input type="text" className='h-9 w-72' placeholder='Leaving from' />   
+            <input type="text" className='h-9 bg-transparent outline-none w-72' placeholder='Leaving from' />   
           </label>
-          <label htmlFor="" className='flex justify-center items-center w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
+          <label htmlFor="" className='flex justify-center items-center mt-3 w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
             <IoLocationOutline className='text-3xl' /> 
-            <input type="text" className='h-9 w-72' placeholder='Going to' />   
+            <input type="text" className='h-9 bg-transparent outline-none w-72' placeholder='Going to' />   
           </label>
-          <label htmlFor="" className='flex justify-center items-center w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
+          <label htmlFor="" className='flex justify-center items-center mt-3 w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
             <BsCalendar4Range className='text-2xl pr-1' /> 
-            <input type="calendar" className='h-9 w-72' placeholder='Date' />   
+            <input type="calendar" className='h-9 bg-transparent outline-none w-72' placeholder='Date' />   
           </label>
-      <label htmlFor="" className='flex justify-center items-center w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
-        <IoPersonOutline className='text-2xl pr-1' /> 
-        <input type="text" className='h-9 w-72' placeholder='2 travelers, 1 room' />   
-      </label>
-      <button type='button' className='bg-blue-600 text-white m-1 w-80 lg:w-32 h-12 rounded-3xl hover:bg-blue-800 duration-700'>Search</button>
-     </form>
+          <label htmlFor="" className='flex justify-center items-center mt-3 w-80 lg:w-56 xl:w-64 h-12 m-1 border-2 rounded-md border-slate-600'>
+            <IoPersonOutline className='text-2xl pr-1' /> 
+            <input type="text" className='h-9 bg-transparent outline-none w-72' placeholder='2 travelers, 1 room' />   
+          </label>
+          <button type='button' className='bg-blue-600 text-white mt-3 mb-11 m-1 w-80 lg:w-32 h-12 rounded-3xl hover:bg-blue-800 duration-700'>Search</button>
+        </form>
      </div>
     </section>
   )
